587c8fff6ce06ec7ab349558382191c6d6e50995
Bill Wong <b6wong@gmail.com>
2020-06-29 14:43:55 -0400

    #84cmr9 Basic react-table

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from "react";
 import GameDataService from "../services/GameService";
-import { Link } from "react-router-dom";
+import { useTable } from "react-table";
+
+const columns = [
+    {
+        Header: "Player 1",
+        columns: [
+            {
+                Header: "Player 1",
+                accessor: "player1"
+            },
+            {
+                Header: "Score 1",
+                accessor: "score1"
+            }
+        ]
+    },
+    {
+        Header: "Player 2",
+        columns: [
+            {
+                Header: "Score 2",
+                accessor: "score2"
+            },
+            {
+                Header: "Player 2",
+                accessor: "player2"
+            }
+        ]
+    },
+    {
+        Header: "Date",
+        columns: [
+            {
+                Header: "Date",
+                accessor: "date"
+            }
+        ]
+    }
+];
 
 const GamesList = () => {
-    const [games, setGames] = useState([]);
-    const [currentGame, setCurrentGame] = useState(null);
-    const [currentIndex, setCurrentIndex] = useState(-1);
-    const [searchPlayer, setSearchPlayer] = useState("");
-    const [showRecord, setShowRecord] = useState(false);
+    const [data, setGames] = useState([]);
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable({
+        columns,
+        data
+    })
 
     useEffect(() => {
         retrieveGames();
     }, []);
-
-    const onChangeSearchPlayer = e => {
-        const searchPlayer = e.target.value;
-        setShowRecord(false);
-        setSearchPlayer(searchPlayer);
-    };
 
     const retrieveGames = () => {
         GameDataService.getAll()
@@ -30,165 +68,32 @@ const GamesList = () => {
             });
     };
 
-    const refreshList = () => {
-        retrieveGames();
-        setCurrentGame(null);
-        setCurrentIndex(-1);
-    };
-
-    const setActiveGame = (game, index) => {
-        setCurrentGame(game);
-        setCurrentIndex(index);
-    };
-
-    const removeAllGames = () => {
-        GameDataService.removeAll()
-            .then(response => {
-                console.log(response.data);
-                refreshList();
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    };
-
-    const findByPlayer = () => {
-        GameDataService.findByPlayer(searchPlayer)
-            .then(response => {
-                setGames(response.data);
-                if (searchPlayer) {
-                    setShowRecord(true);
-                }
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    };
-
     return (
         <div className="list row">
-            <div className="col-md-8">
-                <div className="input-group mb-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Search by player"
-                        value={searchPlayer}
-                        onChange={onChangeSearchPlayer}
-                    />
-                    <div className="input-group-append">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="button"
-                            onClick={findByPlayer}
-                        >
-                            Search
-                </button>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-6">
-                <h4>Games
-                {
-                    showRecord ?
-                    "- W: " + games.filter(game => 
-                        (game.player1.toLowerCase() === searchPlayer.toLowerCase() && game.result === "1") ||
-                        (game.player2.toLowerCase() === searchPlayer.toLowerCase() && game.result === "2")).length +
-                    "  L: " + games.filter(game => 
-                        (game.player1.toLowerCase() === searchPlayer.toLowerCase() && game.result === "2") ||
-                        (game.player2.toLowerCase() === searchPlayer.toLowerCase() && game.result === "1")).length +
-                    "  T:" + games.filter(game => game.result === "T").length
-                    : ""
-                }
-                </h4>
-                <ul className="list-group">
-                    {games &&
-                        games.map((game, index) => (
-                            <li
-                                className={
-                                    "list-group-item " 
-                                    + (index === currentIndex ? "active" : "") 
-                                    + (game.player1.toLowerCase() === searchPlayer.toLowerCase() && game.result === "1" && showRecord ? " won" : "") 
-                                    + (game.player2.toLowerCase() === searchPlayer.toLowerCase() && game.result === "2" && showRecord ? " won" : "") 
-                                    + (game.player1.toLowerCase() === searchPlayer.toLowerCase() && game.result === "2" && showRecord ? " lost" : "") 
-                                    + (game.player2.toLowerCase() === searchPlayer.toLowerCase() && game.result === "1" && showRecord ? " lost" : "") 
-                                    + (game.result === "T" ? " tie" : "")
-                                }
-                                onClick={() => setActiveGame(game, index)}
-                                key={index}
-                            >
-                                {
-                                    game.player2.toLowerCase() === searchPlayer.toLowerCase() ?
-                                    `${game.player2} (${game.score2}) - (${game.score1}) ${game.player1}` :
-                                    `${game.player1} (${game.score1}) - (${game.score2}) ${game.player2}`
-                                }
-                            </li>
-                        ))}
-                </ul>
-
-                <button
-                    className="m-3 btn btn-sm btn-danger"
-                    onClick={removeAllGames}
-                >
-                    Remove All
-            </button>
-            </div>
-            <div className="col-md-6">
-                {currentGame ? (
-                    <div>
-                        <h4>Game</h4>
-                        <div>
-                            <label>
-                                <strong>Player 1:</strong>
-                            </label>{" "}
-                            {currentGame.player1}
-                        </div>
-                        <div>
-                            <label>
-                                <strong>Score:</strong>
-                            </label>{" "}
-                            {currentGame.score1}
-                        </div>
-                        <div>
-                            <label>
-                                <strong>Player 2:</strong>
-                            </label>{" "}
-                            {currentGame.player2}
-                        </div>
-                        <div>
-                            <label>
-                                <strong>Score:</strong>
-                            </label>{" "}
-                            {currentGame.score2}
-                        </div>
-                        <div>
-                            <label>
-                                <strong>Result</strong>
-                            </label>{" "}
-                            {currentGame.result}
-                        </div>
-                        <div>
-                            <label>
-                                <strong>Date:</strong>
-                            </label>{" "}
-                            {currentGame.date}
-                        </div>
-
-                        <Link
-                            to={"/games/" + currentGame.id}
-                            className="badge badge-warning"
-                        >
-                            Edit
-                </Link>
-                    </div>
-                ) : (
-                        <div>
-                            <br />
-                            <p>Please click on a Game...</p>
-                        </div>
-                    )}
-            </div>
+            
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 };
